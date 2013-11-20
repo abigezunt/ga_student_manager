219714bad78ab1e84e9f3057aba4263f392c3568
Abby Howell <abby.g.howell@gmail.com>
2013-11-20 11:33:55 -0500

    updated assignment model to include contributions

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -14,9 +14,7 @@
 
 class Assignment < ActiveRecord::Base
 	belongs_to :course
-	belongs_to :user
-	has_many :finished_assignments, class_name: "Assignment", foreign_key: "assignment_id"
-	belongs_to :master_assignment, class_name: "Assignment"
+	has_many :students, :through => :contributions, class_name: 'User' 
   validates_inclusion_of :assignment_type, :in => %w[quiz homework project]
 	
 	def quiz
