a0bf946482af8518128a3aae631c8e277efe4b7f
Abby Howell <abby.g.howell@gmail.com>
2013-11-21 17:07:41 -0500

    added role method in model so that you can call abby.role and it returns student

--- app/models/user.rb
+++ app/models/user.rb
@@ -55,10 +55,10 @@ class User < ActiveRecord::Base
   # scope :with_role, lamda{|role| includes(:course_membership).where(:course_memberships => {:role => role}) }
   # User.with_role "Student" 
   
-  # def role
-  #         CourseMembership.current.where(user_id: self.id).role
-  #         #self.course_membership.current.role
-  # end
+  def role
+    CourseMembership.where(user_id: self.id).take.role
+    #self.course_membership.current.role
+  end
 
   # def current_course
   #         CourseMembership.current.where(user_id: self.id).course
